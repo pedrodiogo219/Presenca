c6d0ccc78177dfde112df31a364bb82ff03faf2c
Augusto <augui2009@gmail.com>
2019-01-04 17:25:37 -0200

    04/01/18 17:25

--- app/models/forms.py
+++ app/models/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField ,DateField, StringField, PasswordField, BooleanField, FileField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class PreForm(FlaskForm):
-    cpf = StringField("cpf", validators=[DataRequired()])
+    cpf = StringField("cpf", validators=[DataRequired(message="Campo Obrigatório!"), Length(min=14, max=14, message="CPF invalido")])
     prof = StringField('prof', validators=[DataRequired()])
     preSubmit = SubmitField("preSubmit")
 
 class ProfForm(FlaskForm):
-    data = StringField("data", validators=[DataRequired()])
-    sala = StringField("sala", validators=[DataRequired()])
+    data = StringField("data", validators=[DataRequired(message="Campo Obrigatório!"), Length(min=10, max=10, message="Data invalida")])
+    sala = StringField("sala", validators=[DataRequired(message="Campo Obrigatório!")])
     profSubmit = SubmitField("profSubmit")
