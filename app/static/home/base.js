141a419d7fef872922310afc94426c10367fa810
Augusto <augui2009@gmail.com>
2019-01-04 16:11:26 -0200

    Validators

--- app/static/home/base.js
+++ app/static/home/base.js
@@ -25,7 +25,7 @@ function formatar(mascara, documento){
   }
 }
 $(document).ready(function(){
-		var date_input=$('input[name="date"]'); //our date input has the name "date"
+		var date_input=$('input[id="date"]'); //our date input has the name "date"
 		var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
 		date_input.datepicker({
 			format: 'dd/mm/yyyy',
