820f0bd26dfe7fc8ab18256c9a6c4027870fd54a
pedrodiogo219 <pedrodiogo219@gmail.com>
2019-01-22 18:00:44 -0200

    implemented drop down field in index page

--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -36,3 +36,4 @@ def trataCpf(cpf):
             r+=letra
 
     return r
+
--- app/models/forms.py
+++ app/models/forms.py
@@ -2,17 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField ,DateField, StringField, PasswordField, BooleanField, FileField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length
 
-
 class PreForm(FlaskForm):
     cpf = StringField("cpf", validators=[DataRequired(message="Campo Obrigatório!"), Length(min=14, max=14, message="CPF invalido")])
-    prof = StringField('prof', validators=[DataRequired()])
+    prof = SelectField('prof', choices=[])
     preSubmit = SubmitField("preSubmit")
 
 class ProfForm(FlaskForm):
     data = StringField("data", validators=[DataRequired(message="Campo Obrigatório!"), Length(min=10, max=10, message="Data invalida")])
     horario = StringField("horario", validators=[DataRequired(message="Campo Obrigatório!"), Length(min=5, max=5, message="Horário inválido!")]) # manha, tarde
     nivel = StringField("nivel", validators=[DataRequired(message="Campo Obrigatório!")]) #Iniciante, Iniciado, Intermediario, Avancado I, Avancado II
-    #sala = StringField("sala", validators=[DataRequired(message="Campo Obrigatório!")])
     profSubmit = SubmitField("profSubmit")
 
 class ProfLogin(FlaskForm):
