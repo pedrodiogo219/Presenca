d503eefe347753e55ef742a6569e613366ac3b36
Pedro Diogo <pedrodiogo219@gmail.com>
2019-03-29 16:56:55 -0300

    changed to mysql database

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os.path
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://admin:tstrt12@localhost/presenca'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'ASDASDASDASDASD'
-
-
