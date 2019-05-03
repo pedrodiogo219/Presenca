c447dffa79e7dc97b8c619a9be1a8061b3be8f18
Pedro Diogo <pedrodiogo219@gmail.com>
2019-05-03 17:20:16 -0300

    usign DATABASE_URL enviroment variable

--- config.py
+++ config.py
@@ -5,9 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-#SQLALCHEMY_DATABASE_URI = "postgres://zevzrrqiikbhhc:2371d50a04cf84207eac2807f125f5dd8ef3588c709fed686760c44dbf8f676c@ec2-23-21-129-125.compute-1.amazonaws.com:5432/df00kqhp7setue"
-#SQLALCHEMY_DATABASE_URI = "postgres://pediogo:tstrt12@localhost/pediogo"
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
+#SQLALCHEMY_DATABASE_URI = "postgres://usrpresenca:tstrt12@localhost/presenca"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'ASDASDASDASDASD'
