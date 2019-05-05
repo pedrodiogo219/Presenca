cc0d9cb1f78f7d9ca87f24956f39252c69cd9480
Pedro Diogo <pedrodiogo219@gmail.com>
2019-05-04 21:54:44 -0300

    created migrations for the real database

--- migrations/versions/4dcdb9cbd802_.py
+++ migrations/versions/4dcdb9cbd802_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 12bf3a3f9364
+Revision ID: 4dcdb9cbd802
 Revises: 
-Create Date: 2019-05-04 21:23:05.532588
+Create Date: 2019-05-04 21:52:59.548243
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '12bf3a3f9364'
+revision = '4dcdb9cbd802'
 down_revision = None
 branch_labels = None
 depends_on = None
